bd29d66effce4eb24fe84ae0038f3e5c1361f925
Jakub <jakub.sobczynski188@gmail.com>
2019-07-29 14:12:21 +0200

    Updates

---
+++ precache-manifest.bd216e58e4f7dcdd24e0e3f7871e4bba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d9d3dd0e4eb345f9ce8a869cf1ff99a",
+    "url": "/work-organizer/index.html"
+  },
+  {
+    "revision": "16021b5733591765ce47",
+    "url": "/work-organizer/static/css/2.8042e004.chunk.css"
+  },
+  {
+    "revision": "1814aebd8effd6eb9e05",
+    "url": "/work-organizer/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "16021b5733591765ce47",
+    "url": "/work-organizer/static/js/2.2fdd7a6f.chunk.js"
+  },
+  {
+    "revision": "1814aebd8effd6eb9e05",
+    "url": "/work-organizer/static/js/main.76dfacc6.chunk.js"
+  },
+  {
+    "revision": "eefbd774ba603a458153",
+    "url": "/work-organizer/static/js/runtime~main.9734a684.js"
+  }
+]);
