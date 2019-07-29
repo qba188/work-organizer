e0d0db119c9c6020badf0eb7e6cfbea9af749b78
Jakub Sobczyński <jakub.sobczynski188@gmail.com>
2019-07-29 22:18:46 +0200

    components

---
+++ build/precache-manifest.30da760b06fcaa70ad6939658463eacc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a5560fff245757dfbec5d6ec79a801f",
+    "url": "/work-organizer/index.html"
+  },
+  {
+    "revision": "7e099a4b2cd814d83ebc",
+    "url": "/work-organizer/static/css/2.b83189dc.chunk.css"
+  },
+  {
+    "revision": "bc132920515390dc1a2d",
+    "url": "/work-organizer/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "7e099a4b2cd814d83ebc",
+    "url": "/work-organizer/static/js/2.4d60727a.chunk.js"
+  },
+  {
+    "revision": "bc132920515390dc1a2d",
+    "url": "/work-organizer/static/js/main.f67e19d6.chunk.js"
+  },
+  {
+    "revision": "eefbd774ba603a458153",
+    "url": "/work-organizer/static/js/runtime~main.9734a684.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import './index.css';
 import Calendar from 'react-calendar';
 import Clock from 'react-clock';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import {Container, Row, Col} from 'react-bootstrap';
+import {Container, Row, Col, DropdownButton, Dropdown} from 'react-bootstrap';
+
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 class MyCalendar extends React.Component {
     state = {
@@ -23,7 +26,7 @@ class MyCalendar extends React.Component {
         </div>
       );
     }
-  }
+}
 
 class MyClock extends React.Component {
     state = {
@@ -46,7 +49,7 @@ class MyClock extends React.Component {
         </div>
       );
     }
-  }
+}
 
 class DateTime extends MyClock {
 
@@ -62,6 +65,109 @@ class DateTime extends MyClock {
         </div>
       );
     }
+}
+
+class MainDataPicker extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        startDate: new Date()
+      };
+      this.handleChange = this.handleChange.bind(this);
+    }
+  
+    handleChange(date) {
+      this.setState({
+        startDate: date
+      });
+    }
+  
+    render() {
+      return (
+       <div>
+         <DatePicker
+            selected={this.state.startDate}
+            onChange={this.handleChange}
+            showTimeSelect
+            timeFormat="H:MM"
+            timeIntervals={15}
+            dateFormat="MMMM d, yyyy H:MM"
+            timeCaption="time"
+        />
+       </div> 
+      );
+    }
+}
+
+class MainPerson extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        person : "Imię, nazwisko"
+      };
+      this.handleChange = this.handleChange.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({
+          person: event.target.value,
+      })
   }
-ReactDOM.render(<DateTime />, document.getElementById('root'));
+  
+    render() {
+      return (
+       <div>
+         <input value={this.state.person} onChange={this.handleChange}></input>
+       </div> 
+      );
+    }
+}
+
+class MainTask extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        task : "Zadanie"
+      };
+      this.handleChange = this.handleChange.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({
+          task: event.target.value,
+      })
+  }
+  
+    render() {
+      return (
+       <div>
+         <textarea value={this.state.task} onChange={this.handleChange}></textarea>
+       </div> 
+      );
+    }
+}
+
+class DropdownB extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        low : "low",
+        medium : "medium",
+        high : "high"
+      }
+    };
+
+    render() {
+      return (
+       <div>
+        <DropdownButton id="dropdown-basic-button" title="Priorytet">
+          <Dropdown.Item value={this.state.low}>Niski</Dropdown.Item>
+          <Dropdown.Item value={this.state.medium}>Średni</Dropdown.Item>
+          <Dropdown.Item value={this.state.high}>Wysoki</Dropdown.Item>
+        </DropdownButton>
+       </div> 
+      );
+    }
+}
+ReactDOM.render(<MainTask />, document.getElementById('root'));
 
